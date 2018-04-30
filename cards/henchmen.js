c371e217f9c2ec78a476a8f44098ab64727abf90
Michal Szafranski <szafranski@gmail.com>
2018-04-30 08:24:07 +0200

    WIP henchmen

---
+++ cards/henchmen.js
@@ -0,0 +1,26 @@
+addTemplates("HENCHMEN", "Legendary", [
+// CARDNAME: Doombot Legion
+// FIGHT: Look at the top two cards of your deck. KO one of them and put the other back.
+// ATTACK: 3
+makeHenchmenCard("Doombot Legion", 3, {
+  fight: ev => lookAtDeckEv(ev, 2, ev => selectCardAndKOEv(ev, ev.revealed))
+}),
+// CARDNAME: Hand Ninjas
+// FIGHT: You get +1 Recruit.
+// ATTACK: 3
+makeHenchmenCard("Hand Ninjas", 3, {
+  fight: ev => addRecruitEvent(ev, 1)
+}),
+// CARDNAME: Savage Land Mutates
+// FIGHT: When you draw a new hand of cards at the end of this turn, draw an extra card.
+// ATTACK: 3
+makeHenchmenCard("Savage Land Mutates", 3, {
+  fight: () => addEndDrawMod(1)
+}),
+// CARDNAME: Sentinel
+// FIGHT: KO one of your Heroes.
+// ATTACK: 3
+makeHenchmenCard("Sentinel", 3, {
+  fight: ev => selectCardAndKOEv(ev, yourHeroes())
+}),
+]);
