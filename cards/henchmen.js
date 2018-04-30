42746638cc8c275af3216cd8b7ffdbc644c46fab
Michal Szafranski <szafranski@gmail.com>
2018-04-30 13:39:04 +0200

    Fixed henchmen functions

--- cards/henchmen.js
+++ cards/henchmen.js
@@ -3,7 +3,7 @@ addTemplates("HENCHMEN", "Legendary", [
 // FIGHT: Look at the top two cards of your deck. KO one of them and put the other back.
 // ATTACK: 3
 makeHenchmenCard("Doombot Legion", 3, {
-  fight: ev => lookAtDeckEv(ev, 2, ev => selectCardAndKOEv(ev, ev.revealed))
+  fight: ev => lookAtDeckEv(ev, 2, ev => selectCardAndKOEv(ev, playerState.revealed))
 }),
 // CARDNAME: Hand Ninjas
 // FIGHT: You get +1 Recruit.
