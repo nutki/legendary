3a22dea1842815681b67f1ca10da08ebd028690e
Michal Szafranski <szafranski@gmail.com>
2018-05-25 21:55:38 +0200

    Fixed Doom's tactic

--- cards/masterminds.js
+++ cards/masterminds.js
@@ -11,8 +11,8 @@ makeMastermindCard("Dr. Doom", 9, 5, "Doombot Legion", ev => {
   // Choose one: each other player draws a card or each other player discards a card.
   [ "Monarch's Decree", ev => {
       chooseOneEv(ev, "Each other player",
-      "draws a card", ev => eachOtherPlayerVM(p => pickDiscardEv(ev, p)),
-      "discards a card", ev => eachOtherPlayerVM(p => drawEv(ev, 1, p))
+      "draws a card", ev => eachOtherPlayerVM(p => drawEv(ev, 1, p)),
+      "discards a card", ev => eachOtherPlayerVM(p => pickDiscardEv(ev, p))
       );
   } ],
   // Take another turn after this one.
