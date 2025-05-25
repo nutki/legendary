c10e475988814f38dc9c1d223023a3e108301c02
Michal Szafranski <szafranski@gmail.com>
2025-05-25 12:39:01 +0200

    fix #271 - Infiltrate Lair with Spies twist crash

--- cards/schemes.ts
+++ cards/schemes.ts
@@ -442,7 +442,7 @@ makeSchemeCard("Infiltrate the Lair with Spies", { twists: 8 }, ev => {
   // Twist: Put all Bystanders from the Lair into the Overrun pile. Then put a Bystander from next to this Plot into each Lair space under the Bridge, Streets, and Sewers.
   gameState.hq.each(d => d.attached('SPIES').each(c => moveCardEv(ev, c, gameState.escaped)));
   const locations: CityLocation[] = ['BRIDGE', 'STREETS', 'SEWERS'];
-  locations.each(l => withCity(l, d => cont(ev, () => gameState.scheme.attachedDeck("SPIES").withTop(c => attachCardEv(ev, c, d.below, "SPIES")))));
+  locations.each(l => withCity(l, d => cont(ev, () => gameState.scheme.attachedDeck("SPIES").withTop(c => attachCardEv(ev, c, d.above, "SPIES")))));
 }, [escapeProgressTrigger(isBystander), {
   event: 'RECRUIT',
   match: ev => ev.what.location.attached('SPIES').size > 0,
